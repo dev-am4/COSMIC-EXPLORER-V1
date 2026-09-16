ad188b6275cbb448c0530f6cbe07341d45a1b218
dev-am4 <am4.production29@gmail.com>
2026-09-16 21:08:44 +0700

    v9: add runtime loader

---
+++ app-v9.js
@@ -0,0 +1,1 @@
+(()=>{const joined=(window.__V9_B64||[]).join('');const bin=atob(joined);const bytes=Uint8Array.from(bin,c=>c.charCodeAt(0));const code=new TextDecoder().decode(bytes);new Function(code)();})();
